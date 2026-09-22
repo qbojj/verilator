ee67000497069bf3d91a324b9726dc03daa7cde2
Jakub Janeczko <kuba@ev1.pl>
2026-09-22 16:44:09 +0200

    fix bad arg test

--- test_regress/t/t_flag_values_bad.py
+++ test_regress/t/t_flag_values_bad.py
@@ -12,7 +12,7 @@ import vltest_bootstrap
 test.scenarios('vlt')
 
 test.lint(verilator_flags2=[
-    "--output-split-cfuncs -1", "--output-split-ctrace -1", "--output-split-syms",
+    "--output-split-cfuncs -1", "--output-split-ctrace -1", "--output-split-syms -1",
     "--preproc-token-limit 0", "--reloop-limit -1", "--replication-limit -1"
 ],
           fails=True,
